df7a84c40c0c9389ad2c463bdfe0beeccd5fe7b9
teppeis <teppeis@gmail.com>
2020-01-05 01:26:27 +0900

    fix: bump.js

--- bump.js
+++ bump.js
@@ -1,13 +1,13 @@
-'use strict';
+"use strict";
 
-const execa = require('execa');
-const semver = require('semver');
+const execa = require("execa");
+const semver = require("semver");
 
 (async () => {
-	const {stdout} = await execa(__dirname + '/gradlew', ['-q', 'showVersion']);
+  const { stdout } = await execa(`${__dirname}/gradlew`, ["-q", "showVersion"]);
   let [, clutzVersion, clutzGitCommit, closureVersion] = /^([\d.]+):(\w+):(.+)$/.exec(stdout);
   const clutzSemver = semver.coerce(clutzVersion);
-  const {major, minor, patch, prerelease} = semver(require(__dirname + '/package.json').version);
+  const { major, minor, patch, prerelease } = semver(require(`${__dirname}/package.json`).version);
   if (major === clutzSemver.major && minor === clutzSemver.minor) {
     if (prerelease.length > 0) {
       // bump patch version
@@ -20,10 +20,13 @@ const semver = require('semver');
     // use clutz version
     clutzVersion = clutzSemver.toString();
   }
-  if (closureVersion === '1.0-SNAPSHOT') {
-    closureVersion = 'v' + new Date().toISOString().replace(/\..*$/, '').replace(/[-:]/g, '');
+  if (closureVersion === "1.0-SNAPSHOT") {
+    closureVersion = `v${new Date()
+      .toISOString()
+      .replace(/\..*$/, "")
+      .replace(/[-:]/g, "")}`;
   }
   const newVersion = `${clutzVersion}-${clutzGitCommit}.${closureVersion}`;
-  await execa('npm', ['version', newVersion]);
-  console.log('bump version to v' + newVersion);
+  await execa("npm", ["version", newVersion]);
+  console.log(`bump version to v${newVersion}`);
 })();
